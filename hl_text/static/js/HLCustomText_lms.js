26bbdccded3836456a42c66a26c1eeaf6dc340f0
crivet <c.rivet@louisiana.edu>
2019-02-06 08:35:50 -0600

    added mathjax render to xblock output in lms

--- hl_text/static/js/HLCustomText_lms.js
+++ hl_text/static/js/HLCustomText_lms.js
@@ -3,6 +3,7 @@ function HLCK5_XBlock(runtime, xblock_element) {
     $(function ($) {
 		// runtime code for loading the xblock in the LMS portion of the site
 		// 		i.e. the student view initalization
-
+        
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, "HLCustomText_xblock"]);
     });
 }
