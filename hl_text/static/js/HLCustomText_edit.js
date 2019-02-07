fa49c8fef0da9348bdf57076732f698288881632
crivet <c.rivet@louisiana.edu>
2019-02-07 16:06:17 -0600

    testing course id from runtime var in js

--- hl_text/static/js/HLCustomText_edit.js
+++ hl_text/static/js/HLCustomText_edit.js
@@ -5,10 +5,11 @@
 /* JavaScript for LynxTable XBlock, Studio Side. */
 function HLCK5_XBlockStudio(runtime, xblock_element) {
     console.log("runtime:");
-    console.log(runtime);
+    console.log(runtime.course_id);
 
     console.log("element:");
     console.log(xblock_element);
+
     var isFullscreen = false;
 
     var sHeight = 0;
